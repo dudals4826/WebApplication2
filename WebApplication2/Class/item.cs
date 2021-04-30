733ecdae6693007607b147c32bf6a1575ece757c
young min kim <dudals4826@naver.com>
2021-04-30 17:59:16 +0900

    아이템 추가

---
+++ WebApplication2/Class/item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Class
+{
+    public class item
+    {
+        string Name;
+        string Grade;
+        int StarForce;
+    }
+}
